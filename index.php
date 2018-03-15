f42ecfa4c8703a1ee9bb9962e0b875dcf4d15aa7
sedanik <34286843+sedanik@users.noreply.github.com>
2018-03-15 16:22:50 +0100

    afterparty
    afterparty

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 
 <head>
-  <link href="https://fonts.googleapis.com/css?family=Rammetto+One" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Rammetto+One&amp;subset=latin-ext" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Mina&amp;subset=latin-ext" rel="stylesheet">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
