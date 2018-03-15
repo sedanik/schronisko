9f83a0e0d6c074bee5d2e262edd160d0cd890f3f
sedanik <34286843+sedanik@users.noreply.github.com>
2018-03-15 16:36:23 +0100

    asd
    asd

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="css.css" />
+  <script src="js.js" language=javascript type="text/javascript"></script>
   <title></title>
 </head>
 
