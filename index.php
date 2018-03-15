6a21b96af90dd50d4d324c5876a1369c179583b4
DominikDioufowski <dominiksosnowski.14@wp.pl>
2018-03-15 01:06:45 +0100

    xd

--- index.php
+++ index.php
@@ -1,47 +1,13 @@
 <!DOCTYPE html>
-<html lang="pl">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Schronisko</title>
-    <link href="https://fonts.googleapis.com/css?family=Rammetto+One&amp;subset=latin-ext" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="reset.css" media="all">
-    <script src="js.js"></script>
-    <style>
-        * {
-            font-family: 'Rammetto One', cursive;
-        }
-
-        body {
-            background-color: bisque;
-        }
-
-        .header {
-            text-align: center;
-            color: crimson;
-            font-weight: 700;
-        }
-        
-        .info{
-            text-align: right;
-            margin: 0 30px 0 0;
-        }
-
-        input {
-            margin: 0 0 30px 15px;
-            padding: 10px;
-        }
-    </style>
-
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css.css" />
+  <title></title>
 </head>
+<body>
 
-<body onload="zwierzaki()">
-    <p class="header">WITAMY W SCHRONISKU</p>
-    <p class="info"></p>
-    <form>
-        <input type="button" value="Przyjmij zwierzaka" id="dod" onclick="dodawanie()">
-        <input type="button" value="Oddaj zwierzaka" id="dod" onclick="oddaj()">
-    </form>
-    <div style="text-align: center"><img src="grumpy-cat.jpg" alt="kotek"></div>
 </body>
 </html>
