89217a553c47b8ea689f6ca00970a5963441f38b
sedanik <34286843+sedanik@users.noreply.github.com>
2018-03-15 15:24:43 +0100

    nju
    nju

--- index.php
+++ index.php
@@ -1,27 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <link href="https://fonts.googleapis.com/css?family=Rammetto+One" rel="stylesheet">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="css.css" />
   <title></title>
 </head>
 
 <body>
   <div class="wrapper">
-    <header>
+    <div class="header"><header>
       <p>Tu bendzie super zdjencie</p>
-    </header>
+    </header></div>
 
-  </div>
-  <div class="menu">
-<a href="">Menu1</a>
-<a href="">Menu2</a>
-<a href="">Menu3</a>
-<a href="">Menu4</a>
-    <div>
+
+    <div class="menu">
+      <a href="">Menu1</a>
+      <a href="">Menu2</a>
+      <a href="">Menu3</a>
+      <a href="">Menu4</a>
+    </div>
+      <div id="left">
+<?php include ('lista.html')?>
+      </div>
     </div>
 </body>
 
